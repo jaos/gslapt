6c31a866d1c67677ae2193f5a6e630afcb3fdc8b
Jason Woodward <woodwardj@jaos.org>
2005-02-12 03:34:18 +0000

    blocked out the clean button

--- src/callbacks.c
+++ src/callbacks.c
@@ -874,11 +874,13 @@ void lock_toolbar_buttons(void){
   GtkToolButton *action_bar_update_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_update_button") );
   GtkToolButton *action_bar_upgrade_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_upgrade_button") );
   GtkToolButton *action_bar_dist_upgrade_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_dist_upgrade_button") );
+  GtkToolButton *action_bar_clean_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_clean_button") );
   GtkToolButton *action_bar_execute_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_execute_button") );
 
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_update_button,FALSE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_upgrade_button,FALSE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_dist_upgrade_button,FALSE);
+	gtk_widget_set_sensitive((GtkWidget *)action_bar_clean_button,FALSE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_execute_button,FALSE);
 }
 
@@ -887,11 +889,13 @@ void unlock_toolbar_buttons(void){
   GtkToolButton *action_bar_update_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_update_button") );
   GtkToolButton *action_bar_upgrade_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_upgrade_button") );
   GtkToolButton *action_bar_dist_upgrade_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_dist_upgrade_button") );
+  GtkToolButton *action_bar_clean_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_clean_button") );
   GtkToolButton *action_bar_execute_button = GTK_TOOL_BUTTON( lookup_widget(gslapt,"action_bar_execute_button") );
 
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_update_button,TRUE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_upgrade_button,TRUE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_dist_upgrade_button,TRUE);
+	gtk_widget_set_sensitive((GtkWidget *)action_bar_clean_button,TRUE);
 	gtk_widget_set_sensitive((GtkWidget *)action_bar_execute_button,TRUE);
 }
 
