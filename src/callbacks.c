b3011469c8960cc6128690a11713f84dd8d3c681
Jason Woodward <woodwardj@jaos.org>
2005-10-11 22:12:57 +0000

    added new strings to translations

--- src/callbacks.c
+++ src/callbacks.c
@@ -2965,7 +2965,7 @@ static void display_dep_error_dialog (slapt_pkg_info_t *pkg,guint m, guint c,enu
   g_free(msg);
 
   gtk_label_set_text(GTK_LABEL(lookup_widget(w,"dep_error_install_anyway_warning_label")),
-                     "warning here about installing a package with missing deps");
+                     "Do you want to continue without the required packages?");
 
   gtk_label_set_use_markup (GTK_LABEL(lookup_widget(w,"dep_error_label")),
                             TRUE);
