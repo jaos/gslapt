b7a2038631c386be3010e1fa39340aa417f89d03
Jason Woodward <woodwardj@jaos.org>
2008-03-09 01:32:53 +0000

    removed _LIBINTL_H define

--- src/callbacks.h
+++ src/callbacks.h
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#define _LIBINTL_H
 #include <gtk/gtk.h>
 #include <slapt.h>
 #define RC_LOCATION "/etc/slapt-get/slapt-getrc"
